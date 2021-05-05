eae7e607d39e5f567a5e4e96bf66ab01675289db
FARHAD-11PC\Windows 10 <farhat007321@gmail.com>
2021-05-05 14:12:35 +0500

    initial commit

---
+++ lib/data.dart
@@ -0,0 +1,13 @@
+List<String> images = [
+  "assets/image_04.jpg",
+  "assets/image_03.jpg",
+  "assets/image_02.jpg",
+  "assets/image_01.jpg",
+];
+
+List<String> title = [
+  "Manthoka WaterFall",
+  "Deosai Plane",
+  "Shangrila Resort",
+  "Khaplu Palace",
+];
